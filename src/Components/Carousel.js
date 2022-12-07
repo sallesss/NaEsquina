88e657d3a8cc3fff69bc5b787411353c63c3274f
sales <francicley29@gmai.com>
2022-12-07 11:05:39 -0300

    Banner editado

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import bannerImg from '../icons/ban1.jpg';
 import Carousel from 'react-elastic-carousel';
+import banner from '../icons/ban1.png';
+import ban2 from '../icons/ban2.png';
 import '../App.css';
 
 function Marousel() {
 
 
     return (
-        <Carousel enableAutoPlay autoPlaySpeed={2800}>
-            <img src={bannerImg} alt="banner img" />
-            <img src={bannerImg} alt="banner img" />
+        <Carousel enableAutoPlay autoPlaySpeed={3900}>
+                <img src={banner} alt = 'banner img' />
+                <img src={ban2} alt = 'banner img' />
+                <img src={banner} alt = 'banner img' />
         </Carousel>
     )
 }
